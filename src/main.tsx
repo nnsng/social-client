cab837fb6c8d284ab5311153be7e7814b2cd197f
Sangg <ngocsang1201@gmail.com>
2022-04-10 22:41:12 +0700

    revert update react18

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,14 @@ import { CssBaseline } from '@mui/material';
 import { ApplyTheme } from 'components/common';
 import 'i18n';
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { store } from './app/store';
 import './styles/index.css';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Failed to find the root element');
-
-const root = createRoot(rootElement);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ApplyTheme>
@@ -23,5 +19,6 @@ root.render(
         </BrowserRouter>
       </ApplyTheme>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
